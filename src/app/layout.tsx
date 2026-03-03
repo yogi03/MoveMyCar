6f1aeb7ecc540dae05219c7a98e5028ec704fbdd
yogi03 <yogendrachaurasiya30@gmail.com>
2026-03-03 10:53:14 +0530

    Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({
             }
           }}
         />
+        <Analytics />
       </body>
     </html>
   );
